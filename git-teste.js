dbc7ba5275ab50de6e7436b34bc80c34a32dedd1
geovannabarroscustodio <159851392+geovannabarroscustodio@users.noreply.github.com>
2024-02-15 19:35:38 -0300

    Update git-teste.js

--- git-teste.js
+++ git-teste.js
@@ -1,6 +1,6 @@
 /*1- Git init: para criar repositorio
 2- git add readme.md: Comando para adicionar o arquivo README.md 
-3- git commit -m 'passos':  commitar todos os arquivos que estão na área de staged
+3- git commit -m 'mensagem':  commitar todos os arquivos que estão na área de staged
 4- git branch -M main: indica que a branch primária é a main
 5-git remote add origin url: cria uma ancora entre o repositório remoto e o repositorio local
 6- git push origin main: empurrar todas as modificações armazenadas no histórico de commits 
